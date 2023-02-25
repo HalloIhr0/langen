38651ab1f5417ac83ccb113cf39011bcbd62fb3f
HalloIhr0 <124879136+HalloIhr0@users.noreply.github.com>
2023-02-26 00:20:04 +0100

    Created Langen derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::ItemEnum;
+use syn::{self, Attribute};
+
+#[proc_macro_derive(Langen, attributes(token))]
+pub fn langen_macro_fn(input: TokenStream) -> TokenStream {
+    let base_enum: ItemEnum = syn::parse(input).expect("Langen must be applied to an enum");
+    let name = base_enum.ident;
+
+    for variant in &base_enum.variants {
+        for attrib in &variant.attrs {
+            match attrib.path.get_ident().unwrap().to_string().as_str() {
+                "token" => {
+                    let regex = parse_token(attrib).expect(
+                        format!("Invalid \"token\" argument for \"{}\", expected #[token(\"<regex>\")]", variant.ident).as_str()
+                    );
+                    println!("{} {}", variant.ident, regex);
+                }
+                _ => continue,
+            }
+        }
+    }
+
+    let gen = quote! {
+        impl #name {
+            fn test() {
+                println!("Hello World!");
+            }
+        }
+    };
+    gen.into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn parse_token(attrib: &Attribute) -> Option<String> {
+    match attrib.parse_meta() {
+        Ok(syn::Meta::List(list)) => match list.nested.first() {
+            Some(syn::NestedMeta::Lit(syn::Lit::Str(lit))) => Some(lit.value()),
+            _ => None,
+        },
+        _ => None,
     }
 }
