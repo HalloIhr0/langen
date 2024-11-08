ec0b8fb41def492896ca1fed68b5af5dc3aca297
HalloIhr0 <124879136+HalloIhr0@users.noreply.github.com>
2024-11-08 22:07:13 +0100

    Finite automaton from regex

--- langen_macro/src/lib.rs
+++ langen_macro/src/lib.rs
@@ -1,3 +1,4 @@
+use lexer_automaton::Automaton;
 use proc_macro::TokenStream;
 use regex_syntax::hir::Hir;
 use syn::{Data, DeriveInput, Ident, LitStr};
@@ -15,26 +16,31 @@ pub fn langen_derive(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).unwrap();
     if let Data::Enum(data) = input.data {
         let name = input.ident;
-        let mut regex_parser = regex_syntax::ParserBuilder::default();
+        let regex_parser = regex_syntax::ParserBuilder::default();
 
         let mut tokens = vec![];
 
         for variant in data.variants {
             if let Some(r) = variant.attrs.iter().find_map(|attr| {
                 if attr.path().is_ident("token") {
-                    let t: LitStr = attr
-                        .parse_args()
-                        .expect(&format!("token argument for \"{}\" must be string literal", variant.ident));
+                    let t: LitStr = attr.parse_args().unwrap_or_else(|_| {
+                        panic!(
+                            "token argument for \"{}\" must be string literal",
+                            variant.ident
+                        )
+                    });
                     Some(t)
                 } else {
                     None
                 }
             }) {
-                let regex = regex_parser.build().parse(&r.value()).expect(&format!(
-                    "Invalid regex \"{}\" in token \"{}\"",
-                    r.value(),
-                    variant.ident
-                ));
+                let regex = regex_parser.build().parse(&r.value()).unwrap_or_else(|_| {
+                    panic!(
+                        "Invalid regex \"{}\" in token \"{}\"",
+                        r.value(),
+                        variant.ident
+                    )
+                });
                 tokens.push(Token {
                     ident: variant.ident,
                     regex,
@@ -42,8 +48,8 @@ pub fn langen_derive(input: TokenStream) -> TokenStream {
             }
         }
 
-        panic!("{:#?}", tokens);
-        
+        let automaton = Automaton::from_tokens(&tokens);
+        panic!("{}", automaton.to_graphvis());
     } else {
         panic!("Langen can only be used on enum");
     }
